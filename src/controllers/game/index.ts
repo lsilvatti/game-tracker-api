aaeaecbda79c6f2dfde8ca2788fe4ea7f8ee6650
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-02-16 17:35:48 -0300

    feat(game): implement GameController with addGame method

--- src/controllers/game/index.ts
+++ src/controllers/game/index.ts
@@ -1,5 +1,18 @@
-import { type Request, type Response } from 'express';
+import { type Request, type Response, type NextFunction } from 'express';
+import { type IGameService } from '@services/game/index.js';
 
-export function getGame(req: Request, res: Response) {
-    
+export default function GameController(service: IGameService) { 
+    const addGame = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const game = await service.addGame(req.body);
+            return res.status(201).json(game);
+
+        } catch(error) { 
+            next(error);
+        }
+    }
+
+    return { 
+        addGame
+    }
 }
